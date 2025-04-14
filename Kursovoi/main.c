5429455f948f8a3806aa2850041e219b9225d71b
AleksNikolaev <Aleksnikmaster@gmail.com>
2025-04-14 19:46:05 +0300

    add Kursovoi otchet

--- Kursovoi/main.c
+++ Kursovoi/main.c
@@ -10,89 +10,93 @@ uint32_t num = 0;
 int main(int argc, char **argv){
 	setlocale(LC_ALL, "Rus");
 	char* fileName;
-	
-	for(uint8_t i = 1; i < argc; i++)
+	if(argc == 1)
+		printf("\nДанная программа предназначена для вывода статистики температуры, собранной датчиком за определный период времени.\nДля просмотра полного функционала введите клю '-h'.\n\n");
+	else
 	{
-		char* str = argv[i];
-			
-		if(str[0] == '-')
+		for(uint8_t i = 1; i < argc; i++)
 		{
-			switch(str[1])
+			char* str = argv[i];
+				
+			if(str[0] == '-')
 			{
-				case 'f':
-				{	fileName = argv[i+1];
-					i++;
-					
-					flag.load = 1;
-					num = loadStruct(fileName, info);
-					
-					if(!num)
-					{
-						printf("\nОшибка: не удалось открыть файл %s!\n\n", fileName);
-						flag.load = 0;
-					}
-					break;
-				}
-				case 'm':
+				switch(str[1])
 				{
-					flag.argM = 1;
-
-					if(flag.load)
-					{
-						uint8_t numberMonth = 0;
-						sscanf(argv[i+1],"%d",&numberMonth);
+					case 'f':
+					{	fileName = argv[i+1];
 						i++;
-						if(numberMonth > 0 && numberMonth < 13)
-						{			
-							analyzeMonth(info, month, num, numberMonth);
-							printf("\n%2s%8s%8s%8s", "№", "avrM", "minM", "maxM");
-							printf("\n%2d%7d%8d%8d\n\n",numberMonth, month[0].avr, month[0].min, month[0].max);
+						
+						flag.load = 1;
+						num = loadStruct(fileName, info);
+						
+						if(!num)
+						{
+							printf("\nОшибка: не удалось открыть файл %s!\n\n", fileName);
+							flag.load = 0;
+						}
+						break;
+					}
+					case 'm':
+					{
+						flag.argM = 1;
+
+						if(flag.load)
+						{
+							uint8_t numberMonth = 0;
+							sscanf(argv[i+1],"%d",&numberMonth);
+							i++;
+							if(numberMonth > 0 && numberMonth < 13)
+							{			
+								analyzeMonth(info, month, num, numberMonth);
+								printf("\n%2s%8s%8s%8s", "№", "avrM", "minM", "maxM");
+								printf("\n%2d%7d%8d%8d\n\n",numberMonth, month[0].avr, month[0].min, month[0].max);
+							}
+							else
+								printf("Ошибка: введен не коректный номер месяца!\n\n");
 						}
 						else
-							printf("Ошибка: введен не коректный номер месяца!\n\n");
+							printf("Ошибка: данные не загружены!\n");
+						
+						break;
 					}
-					else
-						printf("Ошибка: данные не загружены!\n");
-					
-					break;
-				}
-				case 'p':
-				{
-					if(flag.load)
-						print(info, num);
-					else
-						printf("Ошибка: данные не загружены!\n");
-					break;
-				}
-				case 'h':
-				{
-					flag.menu = 1;
-					menu();
-					break;
-				}
-				default:
-				{
-					flag.menu = 1;
-					printf("\nОшибка: аргумент %s не поддерживается!\n", str);
-					menu();
+					case 'p':
+					{
+						if(flag.load)
+							print(info, num);
+						else
+							printf("Ошибка: данные не загружены!\n");
+						break;
+					}
+					case 'h':
+					{
+						flag.menu = 1;
+						menu();
+						break;
+					}
+					default:
+					{
+						flag.menu = 1;
+						printf("\nОшибка: аргумент %s не поддерживается!\n", str);
+						menu();
+					}
 				}
 			}
 		}
-	}
-	if(!flag.argM)
-	{
-		if(flag.load)
+		if(!flag.argM)
 		{
-			analyzeMonth(info, month, num, 0);
-			analyzeYear(info, &year, num);
-			
-			printf("\n%2s%8s%8s%8s", "№", "avrM", "minM", "maxM");
-			for(uint8_t i = 0; i < 12; i++)
-				printf("\n%2d%7d%8d%8d",i+1, month[i].avr, month[i].min, month[i].max);
-			printf("\n\nСтатистика за год: avr temp = %d; min temp = %d; max temp = %d\n\n", year.avr, year.min, year.max);
+			if(flag.load)
+			{
+				analyzeMonth(info, month, num, 0);
+				analyzeYear(info, &year, num);
+				
+				printf("\n%2s%8s%8s%8s", "№", "avrM", "minM", "maxM");
+				for(uint8_t i = 0; i < 12; i++)
+					printf("\n%2d%7d%8d%8d",i+1, month[i].avr, month[i].min, month[i].max);
+				printf("\n\nСтатистика за год: avr temp = %d; min temp = %d; max temp = %d\n\n", year.avr, year.min, year.max);
+			}
+			else if (!flag.menu)
+				printf("Ошибка: данные не загружены!\n\n");
 		}
-		else if (!flag.menu)
-			printf("Ошибка: данные не загружены!\n\n");
 	}
 	return 0;
 }
